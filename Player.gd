20aaac9c81630ca37efb60698a1080f533788316
Ivan Yu <yu.ivan0352@gmail.com>
2024-04-09 17:01:57 -0400

    basic movement
    Movement and non walkable complete

--- Player.gd
+++ Player.gd
@@ -10,6 +10,18 @@ func _ready():
 	astar_grid.cell_size = Vector2(16, 16)
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
 	astar_grid.update()
+	
+	for x in tile_map.get_used_rect().size.x:
+		for y in tile_map.get_used_rect().size.y:
+			var tile_position = Vector2i(
+				x + tile_map.get_used_rect().position.x,
+				y + tile_map.get_used_rect().position.y
+			)
+			
+			var tile_data = tile_map.get_cell_tile_data(0, tile_position)
+			
+			if tile_data == null or tile_data.get_custom_data("walkable") == false:
+				astar_grid.set_point_solid(tile_position)
 
 func _input(event):
 	if event.is_action_pressed("move") == false:
