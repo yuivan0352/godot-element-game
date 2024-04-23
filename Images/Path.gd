46f40aa4e73a19e30844a171fe112262484eb8fe
Jerry Weng <mcjerry168@gmail.com>
2024-04-23 01:33:49 -0400

    organized file directory

--- Images/Path.gd
+++ Images/Path.gd
@@ -10,6 +10,7 @@ func _process(_delta):
 func _draw():
 	if character.current_point_path.is_empty():
 		for i in character.test_point_path.size():
+			## set_cell(layer, location, which tilemap, which tile, alternative tile?)
 			tile_map.set_cell(1, character.test_point_path[i], 2, Vector2i(4, 2), 0)
 	else:
 		for i in character.current_point_path.size():
