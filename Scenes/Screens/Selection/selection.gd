d272599f7b6ff65b73de0a6adea36e5792427778
Jerry Weng <mcjerry168@gmail.com>
2025-04-22 17:51:49 -0400

    created basic selection screen
    -added panels in selection screen for our characters to be selected
    -created select functionality through a global signal that passes the selected characters into spawncharacter script

--- Scenes/Screens/Selection/selection.gd
+++ Scenes/Screens/Selection/selection.gd
@@ -1,7 +1,49 @@
 extends Control
 
-func _ready() -> void:
-	pass # Replace with function body.
+var Arius = preload("res://Scenes/Character/Classes/Arius.tscn")
+var Brylla = preload("res://Scenes/Character/Classes/Brylla.tscn")
+var Pyrrha = preload("res://Scenes/Character/Classes/Pyrrha.tscn")
+var Quorral = preload("res://Scenes/Character/Classes/Quorral.tscn")
+var selected_characters = []
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Screens/Lobby/Lobby.tscn")
+
+func _on_go_button_pressed() -> void:
+	SignalBus.characters_selected.emit(selected_characters)
+	get_tree().change_scene_to_file("res://Scenes/Game/World.tscn")
+
+func toggle_select(character):
+	if character in selected_characters:
+		selected_characters.erase(character)
+		return false
+	else:
+		selected_characters.append(character)
+		return true
+
+func _on_arius_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if Arius in selected_characters or selected_characters.size() < 3:
+			var is_selected = toggle_select(Arius)
+			$Team/Arius.modulate = Color(1.8, 1.8, 1.8) if is_selected else Color(1.0, 1.0, 1.0)
+		else:
+			# don't let user select more than 3, display popup or something
+			pass
+
+func _on_brylla_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if Brylla in selected_characters or selected_characters.size() < 3:
+			var is_selected = toggle_select(Brylla)
+			$Team/Brylla.modulate = Color(1.8, 1.8, 1.8) if is_selected else Color(1.0, 1.0, 1.0)
+
+func _on_pyrrha_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if Pyrrha in selected_characters or selected_characters.size() < 3:
+			var is_selected = toggle_select(Pyrrha)
+			$Team/Pyrrha.modulate = Color(1.8, 1.8, 1.8) if is_selected else Color(1.0, 1.0, 1.0)
+
+func _on_quorral_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if Quorral in selected_characters or selected_characters.size() < 3:
+			var is_selected = toggle_select(Quorral)
+			$Team/Quorral.modulate = Color(1.8, 1.8, 1.8) if is_selected else Color(1.0, 1.0, 1.0)
---
+++ Scripts/Utilities/SignalBus.gd
@@ -0,0 +1,2 @@
+extends Node
+signal characters_selected(chars_array)
