35ff100306bf3e68f84de39a206d463e612ced2c
darrencodes0 <darrenliu101@gmail.com>
2025-04-24 12:36:21 -0400

    added healing spells to mage

--- Scenes/Enemy/moves.gd
+++ Scenes/Enemy/moves.gd
@@ -2,7 +2,7 @@ extends Unit
 
 var unit_moves = {
 	"Warrior": ["slash", "cleave", "bow_shot"],
-	"Mage": ["fire_bolt","magic_missles", "slash"],
+	"Mage": ["healing_spell","magic_missles","fire_bolt", "slash"],
 	"Archer": ["bow_shot", "slash"],
 	"Slime": ["slash"]
 }
@@ -40,11 +40,13 @@ func use_ranged_move(attacker):
 		var move_used = false
 		match move:
 			"magic_missles":
-				move_used = await(magic_missles(attacker, attacker.turn_queue, tile_layer_zero))
+				move_used = await(magic_missles(attacker, attacker.turn_queue, tile_layer_zero, 1))
 			"fire_bolt":
 				move_used = await(fire_bolt(attacker, attacker.turn_queue, tile_layer_zero))
 			"bow_shot":
 				move_used = await(bow_shot(attacker, attacker.turn_queue, tile_layer_zero))
+			"healing_spell":
+				move_used = await(healing_spell(attacker, attacker.turn_queue, tile_layer_zero, 1))
 			
 			
 		if move_used:
@@ -127,28 +129,35 @@ func bow_shot(attacker, turn_queue, tile_layer_zero) -> bool:
 			return true
 	return false
 
-func magic_missles(attacker, turn_queue, tile_layer_zero) -> bool:
+func magic_missles(attacker, turn_queue, tile_layer_zero, mana_cost) -> bool:
 	var hit_anyone = false
-	for tile in attacker.circle_tiles:
-		var player = turn_queue.pc_positions.find_key(tile)
-		if player != null:
-			var line = draw_attack_line(attacker, player)
-			await get_tree().create_timer(1.5).timeout
-			var damage = rng.randi_range(1, 3) + rng.randi_range(1, attacker.unit_stats.bewitchment)
-			player.unit_stats.health -= damage
-			print(attacker.unit_stats.name, " is attacking!")
-			print("Player is within range! Attacking ", player.unit_stats.name, " for ", damage, " damage!")
-			print(player.unit_stats.name, " has " + str(player.unit_stats.health), " HP left")
+	#checks if attacker has mana to use this for mana_cost
+	if attacker.unit_stats.mana >= mana_cost:
+		for tile in attacker.circle_tiles:
+			var player = turn_queue.pc_positions.find_key(tile)
+			if player != null:
+				var line = draw_attack_line(attacker, player)
+				await get_tree().create_timer(1.5).timeout
+				var damage = rng.randi_range(1, 3) + rng.randi_range(1, attacker.unit_stats.bewitchment)
+				player.unit_stats.health -= damage
+				print(attacker.unit_stats.name, " is attacking!")
+				print("Player is within range! Attacking ", player.unit_stats.name, " for ", damage, " damage!")
+				print(player.unit_stats.name, " has " + str(player.unit_stats.health), " HP left")
+				attacker.unit_stats.mana -= mana_cost
+				
+				if player.unit_stats.health <= 0:
+					print(player.unit_stats.name, " has been defeated!")
+					turn_queue.pc_positions.erase(player)
+					turn_queue.turn_order.erase(player)
+					player.queue_free()
 
-			if player.unit_stats.health <= 0:
-				print(player.unit_stats.name, " has been defeated!")
-				turn_queue.pc_positions.erase(player)
-				turn_queue.turn_order.erase(player)
-				player.queue_free()
-
-			hit_anyone = true
-			line.queue_free()
-
+				hit_anyone = true
+				line.queue_free()
+	else:
+		print("No mana for magic_missles")
+		print(attacker.unit_stats.mana, " mana left")
+		return hit_anyone
+			
 	return hit_anyone
 	
 func fire_bolt(attacker, turn_queue, tile_layer_zero) -> bool:
@@ -173,22 +182,60 @@ func fire_bolt(attacker, turn_queue, tile_layer_zero) -> bool:
 			return true
 	return false
 
-func healing_spell(attacker, turn_queue, tile_layer_zero) -> bool:
-	for tile in attacker.circle_tiles:
-		var ally = turn_queue.enemy_positions.find_key(tile)
-		if ally != null:
-			var line = draw_attack_line(attacker, ally)
-			await get_tree().create_timer(1.0).timeout
-			var healing_health = rng.randi_range(1, 3) + rng.randi_range(1, attacker.unit_stats.bewitchment)
-			ally.unit_stats.health += healing_health
-			print(attacker.unit_stats.name, " is healing!")
-			print("Healing ", ally.unit_stats.name, " for ", healing_health, " health!")
-			print(ally.unit_stats.name, " has " + str(ally.unit_stats.health), " HP left")
+func healing_spell(attacker, turn_queue, tile_layer_zero, mana_cost) -> bool:
+	if attacker.unit_stats.mana >= mana_cost:
+		for tile in attacker.circle_tiles:
+			var ally = turn_queue.enemy_positions.find_key(tile)
+			if ally != null and ally.unit_stats.health <= ally.unit_stats.max_health/2:
+				var line = draw_attack_line(attacker, ally)
+				await get_tree().create_timer(1.0).timeout
+				var healing_health = rng.randi_range(1, 3) + rng.randi_range(1, attacker.unit_stats.bewitchment)
+				ally.unit_stats.health += healing_health
+				print(attacker.unit_stats.name, " is healing!")
+				print("Healing ", ally.unit_stats.name, " for ", healing_health, " health!")
+				print(ally.unit_stats.name, " has " + str(ally.unit_stats.health), " HP left")
+				attacker.unit_stats.mana -= mana_cost
 
-			line.queue_free()
-			return true
+				line.queue_free()
+				return true
 	return false
 
+func mass_healing(attacker, turn_queue, tile_layer_zero, mana_cost) -> bool:
+	var healed = false
+	var valid_targets = []
+	
+	if attacker.unit_stats.mana >= mana_cost:
+
+		for tile in attacker.circle_tiles:
+			var ally = turn_queue.enemy_positions.find_key(tile)
+			if ally != null and ally.unit_stats.health <= ally.unit_stats.max_health/2:
+				valid_targets.append(ally)
+				
+		if valid_targets.size() >= 2:
+			for tile in attacker.circle_tiles:
+				var ally = turn_queue.enemy_positions.find_key(tile)
+				if ally != null and ally.unit_stats.health <= ally.unit_stats.max_health/2:
+					var line = draw_attack_line(attacker, ally)
+					await get_tree().create_timer(1.0).timeout
+					var healing_health = rng.randi_range(1, 3) + rng.randi_range(1, attacker.unit_stats.bewitchment)
+					ally.unit_stats.health += healing_health
+					print(attacker.unit_stats.name, " is healing!")
+					print("Healing ", ally.unit_stats.name, " for ", healing_health, " health!")
+					print(ally.unit_stats.name, " has " + str(ally.unit_stats.health), " HP left")
+					attacker.unit_stats.mana -= mana_cost
+
+					healed = true
+					line.queue_free()
+		else:
+			return healed
+			
+	else:
+		print("No mana for mass_healing")
+		print(attacker.unit_stats.mana, " mana left")
+		return healed
+			
+	return healed
+	
 func draw_attack_line(attacker: Node2D, target: Node2D) -> Line2D:
 	var line = Line2D.new()
 	line.width = 1
