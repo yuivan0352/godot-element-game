4ac39b52af0dc6933e131bb7b9300c240239e294
darrencodes0 <darrenliu101@gmail.com>
2025-04-29 11:59:34 -0400

    Update moves.gd

--- Scenes/Enemy/moves.gd
+++ Scenes/Enemy/moves.gd
@@ -4,7 +4,7 @@ var unit_moves = {
 	#Put most important move to least important
 	"Warrior": ["Iron Defense","Cleave", "Slash", "Arrow Shot"],
 	"Mage": ["Mass Healing", "Healing Spell","Mage Armor","Magic Missles","Fire Bolt", "Necrotic Touch", "Unarmed Strike"],
-	"Archer": ["Multi-Shot","Arrow Shot", "Stab"],
+	"Archer": ["Multi-Shot","Piercing Shot","Arrow Shot", "Stab"],
 	"Slime": ["Pounce"],
 	"King Slime": ["Royal Reproduction", "Pounce"]
 }
@@ -22,6 +22,8 @@ func use_melee_move(attacker):
 		match move:
 			"Iron Defense":
 				move_used = await(iron_defense(attacker, attacker.turn_queue, 2))
+			"Mage Armor":
+				move_used = await(iron_defense(attacker, attacker.turn_queue, 1))
 			"Necrotic Touch":
 				move_used = await(necrotic_touch(attacker, attacker.turn_queue, 1))
 			"Cleave":
@@ -61,15 +63,17 @@ func use_ranged_move(attacker):
 			"Mage Armor":
 				move_used = await(iron_defense(attacker, attacker.turn_queue, 1))
 			"Magic Missiles":
-				move_used = await(magic_missles(attacker, attacker.turn_queue, 1))
+				move_used = await(magic_missles(attacker, attacker.turn_queue, 2))
 			"Multi-Shot":
-				move_used = await(multi_shot(attacker, attacker.turn_queue, 1))
+				move_used = await(multi_shot(attacker, attacker.turn_queue, 2))
 			"Royal Reproduction":
 				move_used = await(call_reinforcements(attacker, attacker.turn_queue, "Slime", 2))
 			"Fire Bolt":
 				move_used = await(fire_bolt(attacker, attacker.turn_queue))
+			"Piercing Shot":
+				move_used = await(piercing_shot(attacker, attacker.turn_queue, 1))
 			"Arrow Shot":
-				move_used = await(bow_shot(attacker, attacker.turn_queue))
+				move_used = await(arrow_shot(attacker, attacker.turn_queue))
 				
 		if move_used:
 			print(move, " has been used")
@@ -144,7 +148,7 @@ func cleave(attacker, turn_queue) -> bool:
 	return hit_anyone
 
 #Single target normal ranged attack
-func bow_shot(attacker, turn_queue) -> bool:
+func arrow_shot(attacker, turn_queue) -> bool:
 	for tile in attacker.circle_tiles:
 		var player = turn_queue.pc_positions.find_key(tile)
 		if player != null:
@@ -172,6 +176,39 @@ func bow_shot(attacker, turn_queue) -> bool:
 			return true
 	return false
 
+#Ignores armor class for shot
+func piercing_shot(attacker, turn_queue, mana_cost) -> bool:
+	if attacker.unit_stats.mana < mana_cost:
+		return false
+		
+	for tile in attacker.circle_tiles:
+		var player = turn_queue.pc_positions.find_key(tile)
+		if player != null:
+			var line = draw_attack_line(attacker, player)
+			await get_tree().create_timer(1.0).timeout
+			var damage = (rng.randi_range(1, 3) + rng.randi_range(1, attacker.unit_stats.brains))
+			
+			if damage > 0:
+				player.unit_stats.health -= damage
+			else:
+				damage = 0
+				
+			print(attacker.unit_stats.name, " is attacking!")
+			print("Player is within range! Attacking ", player.unit_stats.name, " for ", damage, " damage!")
+			print("Player's armor class: ", player.unit_stats.armor_class)
+			print(player.unit_stats.name, " has " + str(player.unit_stats.health), " HP left")
+			attacker.unit_stats.mana -= mana_cost
+			
+			if player.unit_stats.health <= 0:
+				print(player.unit_stats.name, " has been defeated!")
+				turn_queue.pc_positions.erase(player)
+				turn_queue.turn_order.erase(player)
+				player.queue_free()
+
+			line.queue_free()
+			return true
+	return false
+	
 #Single melee basic attack 
 func necrotic_touch(attacker, turn_queue, mana_cost) -> bool:
 	
