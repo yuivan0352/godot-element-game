3413a9d6fada59349e9a93e134f86355c9110485
Jerry Weng <mcjerry168@gmail.com>
2025-04-29 16:52:19 -0400

    selection anchor updates

--- Scenes/Screens/Lobby/lobby.gd
+++ Scenes/Screens/Lobby/lobby.gd
@@ -5,9 +5,6 @@ func _ready():
 	
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Screens/Selection/Selection.tscn")
-	
-func _on_selection_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Screens/Selection/Selection.tscn")
 
 func _on_options_button_pressed() -> void:
 	pass # Replace with function body.
--- Scenes/Screens/Selection/selection.gd
+++ Scenes/Screens/Selection/selection.gd
@@ -7,7 +7,7 @@ var Quorral = preload("res://Scenes/Character/Classes/Quorral.tscn")
 var selected_characters = []
 
 func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Screens/Lobby/Lobby.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Screens/Start/Start.tscn")
 
 func _on_go_button_pressed() -> void:
 	SignalBus.selected_characters = selected_characters
