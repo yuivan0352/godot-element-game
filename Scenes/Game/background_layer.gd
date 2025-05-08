bc99de13c9ffb48ab7f3888d502ab8629971b7f8
ChandlerN03 <nautachandler@gmail.com>
2025-05-08 11:06:18 -0400

    Added back the background

---
+++ Scenes/Game/background_layer.gd
@@ -0,0 +1,8 @@
+extends TileMapLayer
+'''
+func _ready():
+	var terrain_background_index = 12 + (Global.biomeNum % 3)
+	var background = tile_set.get_pattern(terrain_background_index)
+	
+	set_pattern(Vector2i(0,0),background)
+'''
